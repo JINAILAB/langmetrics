d4ccbd2c0d7948a89c996599250fc3a71071f640
JINYONG PARK <parksonice@naver.com>
2025-09-16 18:28:39 +0900

    버전 0.1.15에서 0.2.4로 업데이트, Python 요구 사항을 3.10에서 3.11 미만으로 변경하고, 의존성 목록을 업데이트했습니다. FairnessScore 클래스를 추가하고, ICDScore 클래스의 F1-score 계산 로직을 개선했습니다.

--- src/langevaluate/quantiative_metrics/__init__.py
+++ src/langevaluate/quantiative_metrics/__init__.py
@@ -2,11 +2,13 @@ from .bertscore import BertScore
 from .efficientscore import EfficientScore
 from .icdscore import ICDScore
 from .medconscore import MedconScore
+from .fairnessscore import FairnessScore
 
 __all__ = [
     "BertScore", 
     "EfficientScore",
     "ICDScore",
-    "MedconScore"
+    "MedconScore",
+    "FairnessScore"
 ]
 
